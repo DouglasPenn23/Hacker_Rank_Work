83bb02a7e08ab3bbe420553375d00bccae6d0d5d
DouglasPenn23 <63472155+DouglasPenn23@users.noreply.github.com>
2021-05-10 09:25:24 -0400

    More Java Solutions

--- Solutions.java
+++ Solutions.java
@@ -16,3 +16,41 @@ public String startOz(String str) {
     
     return result;
   }
+
+
+  /*
+The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation. 
+We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.*/
+
+  // Start sleepIn
+  // Create bool method with parameters of weekdays & vacations
+  public boolean sleepIn(boolean weekday, boolean vacation) {
+      // If it's not a weekday or vacation. 
+      // return true. 
+    if (!weekday | vacation){
+      return true;
+    }
+    // If it is one of those 
+    return false;
+  }
+
+  /* 
+We have two monkeys, a and b, and the parameters aSmile and bSmile indicate if each is smiling. 
+We are in trouble if they are both smiling or if neither of them is smiling. 
+Return true if we are in trouble. */
+
+// Create booleans to hold if they're smiling. 
+public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+    // if Asmile & bSmile = true, return true. 
+    if(aSmile && bSmile){
+      return true;
+    }
+    // if aSmile & bSmile = false. 
+    // return true. 
+    
+    if(!aSmile && !bSmile){ 
+      return true;
+  }
+  return false;
+  }
+  
